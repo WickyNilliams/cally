84f2f875a18898d2ee917f314bdafbc57ec2a543
WickyNilliams <WickyNilliams@users.noreply.github.com>
2025-02-08 20:30:23 +0000

    basics of changelog in docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 import atomico from "@atomico/vite";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { h } from "hastscript";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +17,23 @@ export default defineConfig({
       ...atomico({
         cssLiterals: { postcss: true, minify: true },
       }),
+    ],
+  },
+
+  // this is used for the changelog.
+  // we're importing changelog.md from the root of the repo
+  // so it needs some processing to add slugs and autolinks
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          content: () => h("span.hash", "#"),
+          test: ({ tagName }) => tagName === "h2",
+        },
+      ],
     ],
   },
 });
