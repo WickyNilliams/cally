8848c9369576d20708e1e3ebf92e0a6985dd5282
Nick Williams <WickyNilliams@users.noreply.github.com>
2026-03-15 19:17:11 +0000

    fix published types  (#128)
    * Add attw type checking to CI
    * Add tsconfig.build.json specifically for the dts plugin

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
     dts({
       rollupTypes: true,
       bundledPackages: ["atomico"],
+      tsconfigPath: "./tsconfig.build.json",
 
       // workaround to include the global types
       async afterBuild() {
